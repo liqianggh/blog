b401249a155209e07261baf74061e1171e453663
LiQiang <liqiang1chn@163.com>
2018-03-22 11:42:31 +0800

    test

--- src/main/java/cn/blog/common/myInteceptor/CrossInterceptor.java
+++ src/main/java/cn/blog/common/myInteceptor/CrossInterceptor.java
@@ -13,6 +13,8 @@ public class CrossInterceptor extends HandlerInterceptorAdapter {
         response.addHeader("Access-Control-Allow-Methods","*");
         response.addHeader("Access-Control-Max-Age","100");
         response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        String ip = request.getLocalAddr();
+        response.setHeader("server_ip",ip);
         //允许客户端发送cookies
 //        response.addHeader("Access-Control-Allow-Credentials","true");
         return super.preHandle(request, response, handler);
