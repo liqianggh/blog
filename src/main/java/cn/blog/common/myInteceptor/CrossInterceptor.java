342be89704c95889d752e0b3c62b3320c79be7fa
LiQiang <liqiang1chn@163.com>
2018-04-03 10:07:06 +0800

    页面优化

--- src/main/java/cn/blog/common/myInteceptor/CrossInterceptor.java
+++ src/main/java/cn/blog/common/myInteceptor/CrossInterceptor.java
@@ -3,9 +3,13 @@ package cn.blog.common.myInteceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.blog.util.RedisShardedPoolUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 //HandlerInterceptorAdapter
+@Slf4j
 public class CrossInterceptor extends HandlerInterceptorAdapter {
+
 
    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 //        response.addHeader("Access-Control-Allow-Origin","http://58.87.104.109:8080"); 
@@ -17,6 +21,12 @@ public class CrossInterceptor extends HandlerInterceptorAdapter {
 //        response.addHeader("Server",ip);
         //允许客户端发送cookies
 //        response.addHeader("Access-Control-Allow-Credentials","true");
+        //获取客户端ip 并存入redis
+//        String remoteAddr =request.getHeader("X-Real-Ip");
+//        if(null!=remoteAddr){
+//            RedisShardedPoolUtil.setNx("")
+//        }
+
         return super.preHandle(request, response, handler);
     } 
 
