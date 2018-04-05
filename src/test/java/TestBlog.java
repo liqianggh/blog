56d71b1b3a8228334a458dfb86d8d00af1d798e6
LiQiang <liqiang1chn@163.com>
2018-04-05 12:30:12 +0800

    deploy

--- src/test/java/TestBlog.java
+++ src/test/java/TestBlog.java
@@ -1,8 +1,26 @@
 import org.junit.Before;
 
+/**
+ * alt + num 展开指定窗口
+ *
+ * 项目于之间跳转alt +ctrl +[/]
+ * 查看快捷键ctrl shift + a
+ * 查看最近文件ctrl+e
+ * 最近编辑文件 ctrl +shift +e
+ *
+ *上一次修改焦点ctrl+q
+ * 上一次浏览焦点 alt+ 左右箭头
+ *
+ * 代码添加书签：ctrl alt shift +f11 选择一个编号
+ * ctrl+编号  跳转到指定书签
+ *
+ * alt + shfit +f 收藏整个类
+ * 选择函数  alt+shift +f 收藏函数
+ */
 public class TestBlog {
 
-    @Before
-    public void dummy() {
+    public void test1(){
+
     }
+
 }
