7baf48a1cdd9683497889de69d409865466197ea
Jann Lee <liqiang1chn@163.com>
2018-11-14 22:20:40 +0800

    project init

---
+++ src/test/java/cn/mycookies/MyblogApplicationTests.java
@@ -0,0 +1,19 @@
+package cn.mycookies;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Base64Utils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class MyblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+     }
+
+}
