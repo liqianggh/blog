dbc6f9f227c4f2312a96d3c0b22dcf084068db9f
Jann Lee <liqiang1chn@163.com>
2018-11-15 23:58:50 +0800

    项目初始化

--- src/test/java/cn/mycookies/MyblogApplicationTests.java
+++ src/test/java/cn/mycookies/MyblogApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Base64Utils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
