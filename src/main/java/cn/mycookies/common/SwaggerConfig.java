8b83534f8b54aa6f76a425a391cbed3ca2d51d9c
Jann Lee <liqiang1chn@163.com>
2018-11-17 22:52:13 +0800

    NewFeature: SpringBoot继集成Swagger

--- src/main/java/cn/mycookies/common/SwaggerConfig.java
+++ src/main/java/cn/mycookies/common/SwaggerConfig.java
@@ -1,10 +1,56 @@
 package cn.mycookies.common;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @ClassName SwaggerConfig
- * @Description TODO
+ * @Description swagger相关配置
  * @Author Jann Lee
  * @Date 2018-11-17 22:35
  **/
+
+@EnableSwagger2
+@Configuration
 public class SwaggerConfig {
-}
+
+        @Bean
+        public Docket createRestApi() {
+            ParameterBuilder tokenPar = new ParameterBuilder();
+            List<springfox.documentation.service.Parameter> pars = new ArrayList<>();
+            tokenPar.name("token").description("令牌")
+                    .modelRef(new ModelRef("string")).parameterType("query").required(false).build();
+            pars.add(tokenPar.build());
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .apiInfo(apiInfo())
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("cn.mycookies"))
+                    .paths(PathSelectors.any())
+                    .build().globalOperationParameters(pars)  ;
+        }
+
+        @SuppressWarnings("deprecation")
+        private ApiInfo apiInfo() {
+            return new ApiInfoBuilder()
+                    .title("李强的个人博客")
+                    .description("博客相关api")
+                    .termsOfServiceUrl("http://www.mycookies.cn")
+                    .contact("Jann Lee")
+                    .version("3.0")
+                    .build();
+        }
+
+    }
--- src/main/java/cn/mycookies/controller/HelloController.java
+++ src/main/java/cn/mycookies/controller/HelloController.java
@@ -1,10 +1,25 @@
 package cn.mycookies.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName HelloController
- * @Description TODO
+ * @Description 测试
  * @Author Jann Lee
  * @Date 2018-11-17 21:09
  **/
+@RestController
+@RequestMapping("blog")
+@Api("helloworld")
 public class HelloController {
+
+    @GetMapping("hello")
+    @ApiOperation(value = "hello", notes = "A demo of myblog")
+    public String helloworld(){
+        return "hello !";
+    }
 }
