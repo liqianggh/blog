ce50cd20e8e0b7ba6cb8f736d2cd8c5b571a4324
Jann Lee <liqiang1chn@163.com>
2018-11-17 22:51:06 +0800

    misc: 配置端口

---
+++ src/main/java/cn/mycookies/common/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package cn.mycookies.common;
+
+/**
+ * @ClassName SwaggerConfig
+ * @Description TODO
+ * @Author Jann Lee
+ * @Date 2018-11-17 22:35
+ **/
+public class SwaggerConfig {
+}
---
+++ src/main/java/cn/mycookies/controller/HelloController.java
@@ -0,0 +1,10 @@
+package cn.mycookies.controller;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author Jann Lee
+ * @Date 2018-11-17 21:09
+ **/
+public class HelloController {
+}
