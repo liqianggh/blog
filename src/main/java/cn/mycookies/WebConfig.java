eab79cb7e2dfec02443c688f188c5b4967731abf
liqiang <liqiang@weli.cn>
2019-04-17 22:48:34 +0800

    Enhancement: 服务端允许跨域配置

---
+++ src/main/java/cn/mycookies/WebConfig.java
@@ -0,0 +1,23 @@
+package cn.mycookies;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 服务端允许跨域配置
+ * @author Jann Lee
+ * @description TODO
+ * @date 2019-04-17 22:44
+ **/
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
