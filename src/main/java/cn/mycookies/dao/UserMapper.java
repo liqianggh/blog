8ffb0c6f61a803043b046240e13e50e7cb044a88
liqianggh <liqiang1chn@163.com>
2019-07-06 01:52:24 +0800

    Refactor: 评论模块重构

---
+++ src/main/java/cn/mycookies/dao/UserMapper.java
@@ -0,0 +1,31 @@
+package cn.mycookies.dao;
+
+import cn.mycookies.pojo.po.User;
+import cn.mycookies.pojo.po.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/cn/mycookies/pojo/po/User.java
@@ -0,0 +1,24 @@
+package cn.mycookies.pojo.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 用户信息表
+ *
+ * @author Jann Lee
+ * @date 2019-07-06 0:13
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseDO{
+
+    private String userEmail;
+
+    private String userName;
+
+    private Integer userStatus;
+
+}
