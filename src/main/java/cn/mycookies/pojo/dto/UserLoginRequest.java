85e1695e234ac2f517fa32967501bf0fe1b6f42e
liqiang <liqiang@weli.cn>
2019-08-07 18:34:25 +0800

    NewFeature: 整合SpringSecurity，增加登录登出权限控制功能；todo 加密，用户名密码传递

---
+++ src/main/java/cn/mycookies/pojo/dto/UserLoginRequest.java
@@ -0,0 +1,26 @@
+package cn.mycookies.pojo.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 用户登录参数
+ *
+ * @author liqiang
+ * @datetime 2019/8/7 15:36
+ **/
+@Data
+public class UserLoginRequest {
+
+    @NotBlank(message = "用户名不能为空")
+    @Length(min = 3, max = 16,message = "用户名长度应在1-20之间")
+    private String userName;
+
+    @NotBlank(message = "密码不能为空")
+    @Length(min = 3, max = 16,message = "密码长度应在3-16之间")
+    private String password;
+
+}
